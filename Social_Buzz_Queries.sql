c001f602a768af73099616e7a78f8edede3bdfd7
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-10-25 18:48:48 +0800

    Update Social_Buzz_Queries.sql

--- Social_Buzz_Queries.sql
+++ Social_Buzz_Queries.sql
@@ -2,3 +2,18 @@
 -- Create Database
 CREATE DATABASE Social_Buzz;
 
+-- Import the data 
+
+-- Rename the table (beacuse imported table has unwanted characters)
+USE Social_Buzz
+
+EXEC sp_rename Content$, Content 
+EXEC sp_rename ['Reaction Type$'], Reaction_Type
+EXEC sp_rename Reactions$, Reactions
+
+
+--- Figure out the Top 5 performing categories
+SELECT TOP 5 Category, COUNT(*) AS Categories
+FROM Content
+GROUP BY Category
+ORDER BY Category
