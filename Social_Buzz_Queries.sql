c9ce1d9bf48d301e71d9e9a56b22c7549b77d269
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-10-25 19:57:15 +0800

    Update Social_Buzz_Queries.sql

--- Social_Buzz_Queries.sql
+++ Social_Buzz_Queries.sql
@@ -12,12 +12,6 @@ EXEC sp_rename ['Reaction Type$'], Reaction_Type
 EXEC sp_rename Reactions$, Reactions
 
 
---- Figure out the Top 5 performing categories
-SELECT TOP 5 Category, COUNT(*) AS Categories
-FROM Content
-GROUP BY Category
-ORDER BY Category
-
 
 -- Data Joining
 SELECT 
@@ -41,3 +35,31 @@ ON
 	Reac.[Reaction Type] = Rtype.Reaction_Type
 ORDER BY 
 	Reac.F1
+
+
+--- Figure out the Top 5 performing categories
+
+With Social_Buzz AS (
+SELECT 
+	Reac.F1
+	, Reac.[Content ID]
+	, Reac.[Reaction Type]
+	, Reac.Datetime 
+	, Cont.[Content Type]
+	, Cont.Category
+	, Rtype.Sentiment
+	, Rtype.Score
+FROM 
+	Reactions AS Reac
+LEFT JOIN 
+	Content AS Cont
+ON 
+	Reac.[Content ID] = Cont.[Content ID]
+LEFT JOIN 
+	Reaction_Type AS Rtype
+ON 
+	Reac.[Reaction Type] = Rtype.Reaction_Type
+)
+SELECT TOP 5 Category, COUNT(*) AS Category_Count, RANK() OVER (ORDER BY COUNT(Category) DESC) AS Rank
+FROM Social_Buzz
+GROUP BY Category
